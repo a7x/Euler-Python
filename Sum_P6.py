8c1475f21b458ec006542ff4f63ad0c575dd008a
a7x <rohit.dholakia@gmail.com>
2013-12-23 13:05:13 -0800

    Update Sum_P6.py

--- Sum_P6.py
+++ Sum_P6.py
@@ -1,9 +1,9 @@
 #Sum of squares and square of sum for 1 - 100
 import math
-sumSquares=0
-squareSum=0
-sumNum=0
-for i in range(1,101):
-	sumSquares=sumSquares+i*i
-	sumNum=sumNum+i
-print (sumNum*sumNum)-sumSquares
+sumSquares, squareSum, sumNum = 0, 0, 0
+
+for i in xrange(1,101):
+	sumSquares += (i*i)
+	sumNum += i
+	
+print (sumNum*sumNum) - sumSquares
