8d4aaa68b730f1c272bc27c04a8d8d9e6fbd1c2b
a7x <rohit.dholakia@gmail.com>
2013-12-23 13:02:52 -0800

    Update P1.py

--- P1.py
+++ P1.py
@@ -1,13 +1,6 @@
-#Sum of all multiples of 3 or 5 below 1000
 def ifDivisible(n):
-	#return true if divisible by 3 or 5
-	if(n%3==0 or n%5==0):
-		return True
-	else:
-		return False
-sum = 0
-for i in range(3,1000):
-	if(ifDivisible(i)):
-		sum=sum+i
-print sum
+    if n%3 == 0 or n%5 == 0:
+        return True
+    return False
 
+print sum([i for i in xrange(3, 1000) if ifDivisible(i)])
