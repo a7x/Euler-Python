51c0922455c38c5361edce982b41d96360ea4b66
a7x <rohit.dholakia@gmail.com>
2013-12-29 23:09:20 -0800

    Update Googol.py
    removed the ghastliness, if thats even a word.

--- Googol.py
+++ Googol.py
@@ -1,19 +1,9 @@
-#Problem 56
-def power(a,b):
-	#Find a**b
-	print 'a=',a,'b=',b
-	return a**b
-def sumDigits(num):
-	sumdig=0
-	for i in num:
-		sumdig=sumdig+int(i)
-	return sumdig
-maxSum=-1
-print sumDigits(str(power(10,100)))
-print sumDigits(str(power(2,5)))
-for i in range(0,100):
-	for j in range(0,100):
-		sumDig=sumDigits(str(power(i,j)))
-		if(sumDig>maxSum):
-			maxSum=sumDig
-print maxSum
+#newcode 
+def power(a, b):
+    return a ** b
+
+def sum_digits(num):
+    return sum([int(x) for x in str(num)])
+
+
+print max([sum_digits(power(i, j)) for i in xrange(0, 100) for j in xrange(0, 100)])
