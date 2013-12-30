3c84617a9c5c866dbb9d67bd78172bc799d51fac
a7x <rohit.dholakia@gmail.com>
2013-12-29 22:56:52 -0800

    Update Problem52.py
    Replaced boilerplate with Counter !

--- Problem52.py
+++ Problem52.py
@@ -1,28 +1,12 @@
-def ifSame(a,b):
-	if(a>b):
-		larger=a
-		smaller=b
-	else:
-		larger=b
-		smaller=a
-	larger=str(larger)
-	smaller=str(smaller)
-	
-	for i in larger:
-		if(i not in smaller):
-			return False
-	return True
+from collections import Counter
+def ifSame(a, b):
+    return Counter(str(a)) == Counter(str(b))
 
-smallest=True
-num=11
-
-while(smallest):
-	
-	
-	if(ifSame(num,num*2) and ifSame(num,num*3) and ifSame(num,num*4) and ifSame(num,num*5) and ifSame(num,num*6)):
-		print num
-		smallest=False
-	else:
-		num=num+1
-
-
+start = 11
+while True:
+    if ifSame(num, num * 2) and ifSame(num, num * 3) and ifSame(num, num * 4) and ifSame(num, num * 5) and \
+        ifSame(num, num * 6):
+            print num
+            break
+    else:
+        start += 1
