0d0f36494ed65624dd781eaa4e0ec57b08c64940
Sudha Verma <sudha.srm@gmail.com>
2016-04-03 21:23:14 -0700

    Update Problem8.py
    xrange to range, removed empty first line

--- Problem8.py
+++ Problem8.py
@@ -1,4 +1,3 @@
-
 import operator
 print max(reduce(operator.mul, [int(x) for x in large_number[i:i+5]], 1) \
-               for i in xrange(len(large_number)))
+               for i in range(len(large_number)))
